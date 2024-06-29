06e015a7aee72f1b603388b4b36a289a435c05db
kelver <kelverwt@gmail.com>
2024-06-29 13:43:15 -0300

    create service and repository of actors

---
+++ actors/service.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from actors.repository import ActorRepository
+
+
+class ActorService:
+    def __init__(self):
+        self.actor_repository = ActorRepository()
+
+    def get_actors(self):
+        actor = self.actor_repository.get_actors()
+        return actor
+    
+    def create_actor(self, name, birthday, nationality):
+        actor = dict(
+            name=name,
+            birthday=birthday,
+            nationality=nationality,
+        )
+        return self.actor_repository.create_actor(actor)
+        
